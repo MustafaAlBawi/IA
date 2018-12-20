5151861bf1389ac0cd4ebb319f081ec04b0e4c6b
Thomas de Lange <delange.thomas@gmail.com>
2018-12-20 14:35:18 +0100

    core with GoogleC

--- Agent/core.py
+++ Agent/core.py
@@ -5,8 +5,17 @@
 """
 import helpers
 from Classes.Test import Test
+from Classes.GoogleCalendar import GoogleCalendar
+
+"""Start the App"""
 
 def start():
-    """Start the App"""
-    if helpers.getAnswer():
-        print(Test.getHmm())
+        GC = GoogleCalendar('readonly')
+        events = GC.events('primary', GC.now, 10, True, 'startTime')\
+            .get('items', [])
+        
+        if not events:
+            print('No upcoming events found.')
+        for event in events:
+            start = event['start'].get('dateTime', event['start'].get('date'))
+            print(start, event['summary'])
