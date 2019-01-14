7288cb4d7d9bda40961647809685cf065f269a9e
Euan <euanw93@gmail.com>
2019-01-14 13:20:38 +0100

    Event class

---
+++ Agent/Classes/Event.py
@@ -0,0 +1,28 @@
+
+
+
+class Event:# (object)?
+    def __init__(self, owner = None, priority = 1):
+        self.owners_priorities = [owner, priority]
+        self.highest = priority
+
+    def add_owner(self, owner = None, priority = 1):
+        if self.highest < priority:
+            self.highest = priority
+            
+        self.owners_priorities = self.owners_priorities.exteend([owner, priority])
+
+    
+    def getOwnersPriorities(self):
+        return self.owners_priorities
+    
+    def getHighest(self):
+        return self.highest
+
+
+
+
+
+
+
+
