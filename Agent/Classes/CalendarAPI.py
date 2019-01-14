50831b7e5f7517872f85722e570a59afd62c2104
Unknown <tychobuisman@hotmail.com>
2019-01-14 11:30:45 +0100

    CalendarAPI works with datetime dataframe now

--- Agent/Classes/CalendarAPI.py
+++ Agent/Classes/CalendarAPI.py
@@ -10,6 +10,7 @@ class CalendarAPI(object):
     """
     Class that serves the google calendar API
     """
+<<<<<<< Updated upstream
     def __init__(self, scope='readonly'):
         self.SCOPES = 'https://www.googleapis.com/auth/calendar.' + scope
         self.store = file.Storage('token.json')
@@ -67,7 +68,36 @@ class CalendarAPI(object):
         index = range(1, 25)
         return pd.DataFrame(None, index=index, columns=columns)
 
-    def insertEvent(self, df, date, time, priority = 1):
+    def insertEvent(self, df, date, time, priority = 2):
+        eventStart = int(10)
+        eventEnd = int(12)
+        eventfound = 0
+        Date = 0
+        Time = 0
+
+        start_date = '2019-03-04'
+        start_date_object = datetime.datetime(int(start_date[0:4]), int(start_date[5:7]), int(start_date[8:10]))
+        end_date = '2019-03-10'
+        for j in range(int(start_date[8:10]), int(end_date[8:10])):
+            for i in range(eventStart, int(eventEnd) + 1):
+                timeslot = df.get_value(i, start_date_object)
+
+                if timeslot == 0:
+                    df.set_value(int(i), start_date_object, 2)
+                    print(timeslot)
+                    Date = start_date_object
+                    Time = i
+                    print('Date:', Date, 'Time:', i)
+                    print('Planned the event')
+                    eventfound = 1
+                    break
+            if eventfound == 1:
+                break
+            start_date_object += datetime.timedelta(days=1)
+        if eventfound == 0:
+            print('Could not find timeslot')
+        print(df)
+
         # TODO:
         # de colom kan worden opgehaald als string of met een datetime.
         # de rij zijn dan de uuren. De uren waarop een afspraak duurt 
@@ -75,6 +105,39 @@ class CalendarAPI(object):
         column = df[date][time] = priority
         print(column)
 
+    def writeInCalendar():
+        endTime = int(Time)
+        Time = int(Time) - 1
+        startEv = datetime.datetime.combine(start_date_object, datetime.time(Time, 0, 0))
+        endEv = datetime.datetime.combine(start_date_object, datetime.time(endTime, 0, 0))
+        startEv = str(startEv.date()) + 'T' + str(startEv.time()) + 'Z'
+        endEv = str(endEv.date()) + 'T' + str(endEv.time()) + 'Z'
+        print(startEv, endEv)
+
+        # Insert event to google calendar
+        event = {
+            'summary': 'KoffieTest',
+            'location': 'unknown',
+            'description': 'Getting coffee',
+            'start': {
+                'dateTime': startEv,
+                'timeZone': 'Europe/Amsterdam',
+            },
+            'end': {
+                'dateTime': endEv,
+                'timeZone': 'Europe/Amsterdam',
+            },
+            'recurrence': [
+            ],
+            'attendees': [
+            ],
+            'reminders': {
+                'useDefault': True,
+
+            },
+        }
+
+        event = service.events().insert(calendarId='primary', body=event).execute()
 
 # TODO:
 # Code hier onder moet deels nog omgezet worden in d CalendarAPI class.
@@ -189,3 +252,114 @@ class CalendarAPI(object):
 #     'Event created: %s' % (event.get('htmlLink'))
 # if __name__ == '__main__':
 #     main()
+=======
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    store = file.Storage('token.json')
+    creds = store.get()
+    if not creds or creds.invalid:
+        flow = client.flow_from_clientsecrets('credentials.json', SCOPES)
+        creds = tools.run_flow(flow, store)
+    service = build('calendar', 'v3', http=creds.authorize(Http()))
+
+    # Call the Calendar API
+    now = '2019-03-04T00:00:00.00Z' #datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
+    end = '2019-03-10T00:00:00.00Z'
+
+    print('Getting the upcoming 10 events')
+    events_result = service.events().list(calendarId='primary', timeMin=now, timeMax =end ,
+                                        maxResults=10, singleEvents=True,
+                                        orderBy='startTime').execute()
+    events = events_result.get('items', [])
+
+    column = pd.Series(range(4, 10))
+    row = pd.Series(range(0, 25))
+    df = pd.DataFrame(0, index=row, columns=column)
+
+    if not events:
+        print('No upcoming events found.')
+    for event in events:
+        start = event['start'].get('dateTime', event['start'].get('date'))
+        end = event['end'].get('dateTime', event['start'].get('date'))
+        print(start, event['summary'])
+        print(events[0])
+        startDate = start[8:10]
+        startTime = start[11:13]
+        endDate = end[8:10]
+        endTime = end[11:13]
+
+        print(startDate, startTime, endDate, endTime)
+
+        startTime = int(startTime)
+        endTime = int(endTime)
+        loop = endTime - startTime + 1
+        for i in range(0,loop):
+            df.set_value(startTime + i, int(startDate), 1)
+
+    print(df)
+
+    #Plan event
+    eventName = 'koffie'
+    eventStart = int(10)
+    eventDuration = int(1)
+    eventEnd = int(12)
+    eventfound = 0
+    Date = 0
+    Time = 0
+    for j in range(4, 10):
+        for i in range(eventStart,int(eventEnd)+1):
+            timeslot = df.get_value(i,j)
+
+            if timeslot == 0:
+                df.set_value(int(i), int(j), 2)
+                print(timeslot)
+                Date = j
+                Time = i
+                print('Date:', j, 'Time:', i)
+                print('Planned the event')
+                eventfound = 1
+                break
+        if eventfound == 1:
+            break
+
+    if eventfound == 0:
+        print('Could not find timeslot')
+    print(df)
+
+
+    ##WRITE TO GOOGLE CALENDAR
+    endTime = int(Time) - 1
+    Time = int(Time) - 2
+    startEv = '2019-03-0'+str(Date)+'T'+str(Time)+':00:00.00Z'
+    endEv = '2019-03-0'+str(Date)+'T'+str(endTime)+':00:00.00Z'
+    print(startEv)
+    print(endEv)
+    event = {
+        'summary': 'KoffieTest',
+        'location': 'unknown',
+        'description': 'Getting coffee',
+        'start': {
+            'dateTime': startEv,
+            'timeZone': 'Europe/Amsterdam',
+        },
+        'end': {
+            'dateTime': endEv,
+            'timeZone': 'Europe/Amsterdam',
+        },
+        'recurrence': [
+        ],
+        'attendees': [
+        ],
+        'reminders': {
+            'useDefault': True,
+
+        },
+    }
+
+    event = service.events().insert(calendarId='primary', body=event).execute()
+    print
+    'Event created: %s' % (event.get('htmlLink'))
+if __name__ == '__main__':
+    main()
+>>>>>>> Stashed changes
